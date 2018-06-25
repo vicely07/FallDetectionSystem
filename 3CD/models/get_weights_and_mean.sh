2331808aaa36978f1d063720be55bd3316faff5d
vicely07 <38712706+vicely07@users.noreply.github.com>
2018-06-25 16:53:32 -0500

    Update get_weights_and_mean.sh

--- 3CD/models/get_weights_and_mean.sh
+++ 3CD/models/get_weights_and_mean.sh
@@ -4,11 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo ---------------------------------------------
 echo Downloading Sports1mil pre-trained model...
-wget \
-  -N \
-  --content-disposition \
-  http://vlg.cs.dartmouth.edu/c3d/conv3d_deepnetA_sport1m_iter_1900000 \
-  --directory-prefix=${DIR}
+wget -N --content-disposition http://vlg.cs.dartmouth.edu/c3d/conv3d_deepnetA_sport1m_iter_1900000 --directory-prefix=${DIR}
 
 echo ---------------------------------------------
 echo Unpacking mean cube...
