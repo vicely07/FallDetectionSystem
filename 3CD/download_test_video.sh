bfb6f49e3456efe2b11d230e67367ec69b258183
vicely07 <38712706+vicely07@users.noreply.github.com>
2018-06-26 10:21:18 -0500

    Update download_test_video.sh

--- 3CD/download_test_video.sh
+++ 3CD/download_test_video.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-youtube-dl \
-  -f 18 \
-  -o '%(id)s.%(ext)s' \
-  'https://www.youtube.com/watch?v=dM06AMFLsrc'
+youtube-dl -f 18 -o '%(id)s.%(ext)s' 'https://www.youtube.com/watch?v=dM06AMFLsrc'
