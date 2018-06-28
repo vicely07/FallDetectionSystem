644c8768f14ccaba66b2f18bdf8b89572f0150e8
vicely07 <38712706+vicely07@users.noreply.github.com>
2018-06-27 19:52:45 -0500

    Update C3D_model.py

--- 3CD/C3D_model.py
+++ 3CD/C3D_model.py
@@ -49,7 +49,7 @@ def get_model(summary=False):
     model.add(Convolution3D(512, 3, 3, 3, activation='relu', 
                             border_mode='same', name='conv5a',
                             subsample=(1, 1, 1)))
-    model.add(Convolution3D(512, 3, 3, activation='relu', 
+    model.add(Convolution3D(512, 3, 3, 3, activation='relu', 
                             border_mode='same', name='conv5b',
                             subsample=(1, 1, 1)))
    
