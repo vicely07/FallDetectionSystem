0f3f38addfba5b9d98bf83e7a939661cd9027669
vicely07 <38712706+vicely07@users.noreply.github.com>
2018-06-29 18:22:48 -0500

    Update Test_model.py

--- 3CD/Test_model.py
+++ 3CD/Test_model.py
@@ -34,7 +34,7 @@ frames = vid[2000:2016, 8:120, 30:142,: ]
 X = frames.transpose((3, 0, 1, 2))
 output = model.predict_on_batch(np.array([X]))
 
-print('Position of maximum probability: {}'.format(output[0].argmax()))
-print('Maximum probability: {}'.format(max(output[0][0])))
-print('Corresponding label: {}'.format(labels[output[0].argmax()]))
+print('Position of maximum probability: {0}'.format(output[0].argmax()))
+print('Maximum probability: {0:.5f}'.format(max(output[0][0])))
+print('Corresponding label: {0}'.format(labels[output[0].argmax()]))
 
